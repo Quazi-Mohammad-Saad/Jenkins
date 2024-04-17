be70ce83aac8f644641e50c7929fed3ff29ebb4a
Quazi <quazi1996saad@gmail.com>
2024-04-17 13:21:23 +0530

    added the date and time

--- Quazi.java
+++ Quazi.java
@@ -1,8 +1,12 @@
+import java.util.Date;
+
 public class Quazi{
 
 public static void main(String[] args){
 
 	System.out.println("Hi Quazi, How are you? ");
 	System.out.println("This is second line");
+	System.out.println("This is third line printing today date and time: ");
+	System.out.println(new Date());
 }
 }
