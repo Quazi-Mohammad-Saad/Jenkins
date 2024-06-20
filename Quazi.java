510f8879943a028fa5e7c10128717eb2f6134d56
Quazi <quazi1996saad@gmail.com>
2024-06-20 15:38:54 +0530

    20 june updation

--- Quazi.java
+++ Quazi.java
@@ -6,17 +6,19 @@ public class Quazi{
 public static void main(String[] args){
 
 	System.out.println("Hi Quazi, How are you? ");
-	System.out.println("This is second line");
-	System.out.println("This is third line printing today date and time: ");
-	System.out.println(new Date());
-	System.out.println("Adding some more features line number 4: ");
-	ArrayList<Integer> integerArrayList = new ArrayList<>();
-	integerArrayList.add(1);
-	integerArrayList.add(10);
-	integerArrayList.add((100));
-	System.out.println(integerArrayList);
+//	System.out.println("This is second line");
+//	System.out.println("This is third line printing today date and time: ");
+//	System.out.println(new Date());
+//	System.out.println("Adding some more features line number 4: ");
+//	ArrayList<Integer> integerArrayList = new ArrayList<>();
+//	integerArrayList.add(1);
+//	integerArrayList.add(10);
+//	integerArrayList.add((100));
+//	System.out.println(integerArrayList);
 	QuaziNew quaziNew = new QuaziNew();
 	quaziNew.QuaziMethod("Sidra");
-	System.out.println("20 April Updation");
+//	System.out.println("20 April Updation");
+	System.out.println("Update on 20 june 2024");
+	System.out.println("Sidra got married with Anwar");
 }
 }
