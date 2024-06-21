f035e238f6b1bcc5ff9308f566ebb45fd6f9da0f
Quazi <quazi1996saad@gmail.com>
2024-06-21 12:02:26 +0530

    initial commit: updation on 21 june

---
+++ QuaziNew2.java
@@ -0,0 +1,10 @@
+public class QuaziNew2 {
+    public QuaziNew2() {
+        System.out.println("Inside the QuaziNew2() constructor");
+    }
+
+    public void quaziNewMethod(){
+        System.out.println("Inside the quaziNewMethod() ");
+        System.out.println("Hello Quazi , How are you? ");
+    }
+}
